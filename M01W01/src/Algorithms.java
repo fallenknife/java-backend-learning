7e56e210d30a64ba3006db237d25e770bd9c01dd
Roman Belousov <belousowroman2001@gmail.com>
2025-08-27 22:24:01 +0700

    fix: func logic

--- M01W01/src/Algorithms.java
+++ M01W01/src/Algorithms.java
@@ -2,7 +2,7 @@ public class Algorithms {
     static int findMax(int[] arr) {
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (max >= arr[i]) {
+            if (max <= arr[i]) {
                 max = arr[i];
             }
         }
