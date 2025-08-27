9f00e1790a228752196aa172c95d28b02a004e2a
Roman Belousov <belousowroman2001@gmail.com>
2025-08-27 22:10:07 +0700

    feat: findMax implemented

---
+++ M01W01/src/Algorithms.java
@@ -0,0 +1,19 @@
+public class Algorithms {
+    static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max >= arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    static boolean isPrime(int n) {
+
+    }
+
+    static int factorial(int n) {
+
+    }
+}
