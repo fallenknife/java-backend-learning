a21053f9781a6a89f8c19004ca7cff48c70dfc6e
Roman Belousov <belousowroman2001@gmail.com>
2025-08-27 20:42:31 +0700

    feat: impl calculator

--- M01W01/src/Calculator.java
+++ M01W01/src/Calculator.java
@@ -1,33 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number a");
-        int a = scanner.nextInt();
-        System.out.print("Enter number b");
-        int b = scanner.nextInt();
-        System.out.print("Enter operation");
-        char c = scanner.next().charAt(0);
-
-
-        int result;
-        switch (c) {
-            case '/':
-                result = divide(a, b);
-                break;
-            case '*':
-                result = multiply(a, b);
-                break;
-            case '+':
-                result = add(a, b);
-                break;
-            case '-':
-                result = subtract(a, b);
-            default:
-                result = 0;
+        try {
+            System.out.print("Enter number a: ");
+            int a = scanner.nextInt();
+            System.out.print("Enter number b: ");
+            int b = scanner.nextInt();
+            System.out.print("Enter operation: ");
+            char c = scanner.next().charAt(0);
+            System.out.print(c);
+            int result;
+            switch (c) {
+                case '/':
+                    result = divide(a, b);
+                    break;
+                case '*':
+                    result = multiply(a, b);
+                    break;
+                case '+':
+                    result = add(a, b);
+                    break;
+                case '-':
+                    result = subtract(a, b);
+                default:
+                    result = 0;
+                    System.out.printf("Error: unknown operation");
+            }
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Error occured");
         }
-        System.out.println(result);
     }
 
     static int add(int a, int b) {
