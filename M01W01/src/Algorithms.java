fb8baeba5f2a8b544bb6104332b0fcf7cbb45993
Roman Belousov <belousowroman2001@gmail.com>
2025-08-27 22:28:41 +0700

    test: tests for all methods

--- M01W01/src/Algorithms.java
+++ M01W01/src/Algorithms.java
@@ -25,4 +25,18 @@ public class Algorithms {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 3, 4, 5, 6, 8, 9, 11};
+        System.out.print(findMax(ar));
+        System.out.println("");
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(11));
+        System.out.println(isPrime(12));
+        System.out.println(factorial(1));
+        System.out.println(factorial(3));
+        System.out.println(factorial(11));
+    }
 }
