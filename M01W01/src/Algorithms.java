78b5e39ea74960b1ffdae9c264701ae1893b6b5f
Roman Belousov <belousowroman2001@gmail.com>
2025-08-27 22:18:35 +0700

    feat: factorial impl

--- M01W01/src/Algorithms.java
+++ M01W01/src/Algorithms.java
@@ -19,6 +19,10 @@ public class Algorithms {
     }
 
     static int factorial(int n) {
-
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
